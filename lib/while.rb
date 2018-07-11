1007b6c793f2659623ecdf1f9a3391daefb9733a
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-11 16:32:34 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,8 @@
 def using_while
 	levitation_force = 6
-	puts "Wingardium Leviosa"
+
+	while levitation_force
+		puts "Wingardium Leviosa"
 	levitation_force += 1
 	until levitation_force == 10
 end
